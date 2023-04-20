a7b6d5b02629e7e40c77e04cdcf34934fbc2658c
root <mgiraudo@ies21.edu.ar>
2023-04-20 13:54:19 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace PryEjemplo20_04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnComparar = new System.Windows.Forms.Button();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.lblCaracteresA = new System.Windows.Forms.Label();
+            this.lblCaracteresB = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnReemplazar = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnComparar
+            // 
+            this.btnComparar.Location = new System.Drawing.Point(42, 127);
+            this.btnComparar.Name = "btnComparar";
+            this.btnComparar.Size = new System.Drawing.Size(75, 23);
+            this.btnComparar.TabIndex = 0;
+            this.btnComparar.Text = "Comparar";
+            this.btnComparar.UseVisualStyleBackColor = true;
+            this.btnComparar.Click += new System.EventHandler(this.btnComparar_Click);
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(128, 15);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(121, 20);
+            this.txtA.TabIndex = 1;
+            this.txtA.TextChanged += new System.EventHandler(this.txtA_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Texto A";
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(63, 236);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(75, 23);
+            this.btnConvertir.TabIndex = 4;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Texto B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Fecha";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(128, 51);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(121, 20);
+            this.txtB.TabIndex = 7;
+            this.txtB.TextChanged += new System.EventHandler(this.txtB_TextChanged);
+            // 
+            // lblCaracteresA
+            // 
+            this.lblCaracteresA.AutoSize = true;
+            this.lblCaracteresA.Location = new System.Drawing.Point(267, 18);
+            this.lblCaracteresA.Name = "lblCaracteresA";
+            this.lblCaracteresA.Size = new System.Drawing.Size(13, 13);
+            this.lblCaracteresA.TabIndex = 8;
+            this.lblCaracteresA.Text = "0";
+            // 
+            // lblCaracteresB
+            // 
+            this.lblCaracteresB.AutoSize = true;
+            this.lblCaracteresB.Location = new System.Drawing.Point(267, 58);
+            this.lblCaracteresB.Name = "lblCaracteresB";
+            this.lblCaracteresB.Size = new System.Drawing.Size(13, 13);
+            this.lblCaracteresB.TabIndex = 9;
+            this.lblCaracteresB.Text = "0";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(128, 92);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(121, 20);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(383, 181);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 11;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnReemplazar
+            // 
+            this.btnReemplazar.Location = new System.Drawing.Point(147, 156);
+            this.btnReemplazar.Name = "btnReemplazar";
+            this.btnReemplazar.Size = new System.Drawing.Size(75, 23);
+            this.btnReemplazar.TabIndex = 12;
+            this.btnReemplazar.Text = "Reemplazar";
+            this.btnReemplazar.UseVisualStyleBackColor = true;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(476, 183);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(39, 20);
+            this.txtBuscar.TabIndex = 13;
+            this.txtBuscar.Visible = false;
+            this.txtBuscar.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(156, 241);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(42, 13);
+            this.lblFecha.TabIndex = 14;
+            this.lblFecha.Text = "FECHA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 392);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.btnReemplazar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblCaracteresB);
+            this.Controls.Add(this.lblCaracteresA);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.btnComparar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnComparar;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label lblCaracteresA;
+        private System.Windows.Forms.Label lblCaracteresB;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnReemplazar;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Label lblFecha;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PryEjemplo20_04
+{
+    public partial class Form1 : Form
+    {
+        string textoA;
+        string textoB;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void txtA_TextChanged(object sender, EventArgs e)
+        {
+
+
+            
+            textoA = txtA.Text;
+
+            lblCaracteresA.Text = textoA.Length.ToString();
+        }
+
+        private void txtB_TextChanged(object sender, EventArgs e)
+        {
+            
+
+            textoB = txtB.Text;
+            lblCaracteresB.Text = textoB.Length.ToString();
+        }
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            if (textoA.IndexOf(txtA.Text) != -1)
+            {
+                txtBuscar.BackColor = Color.Green;
+            }
+            else
+            {
+                txtBuscar.BackColor = Color.Red;
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            txtBuscar.Visible = true;
+                 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnComparar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            DateTime A = dateTimePicker1.Value;
+            string B = A.ToString("dd/MM/yyyy");
+            lblFecha.Text = B;
+        }
+    }
+}
